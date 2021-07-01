110c6ff4a22d76d61dd2bda8a44289b904d7a1df
lucasangiorama <angioramaluca@gmail.com>
2021-07-01 20:36:16 -0300

    pantalla detalle

--- app.js
+++ app.js
@@ -7,11 +7,26 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
+app.get("/index", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
+app.get("/detalle", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/detalle.html"));
+});
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+app.get("/registro", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/registro.html"));
+});
+app.get("/tienda", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/tienda.html"));
+});
 
-app.listen(3000, function() {
-  console.log("Corriendo servidor en el puerto 3000");
+app.listen(3001, function() {
+  console.log("Corriendo servidor en el puerto 3001");
 }
 )
