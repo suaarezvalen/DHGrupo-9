8f7ab7a2d93741c362fc0adeef0b05a5e310021e
lucasangiorama <angioramaluca@gmail.com>
2021-07-12 19:27:47 -0300

    subiendo a heroku

--- app.js
+++ app.js
@@ -7,26 +7,15 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
+
+const publicPath2 = path.resolve(__dirname, "./views");
+app.use(express.static(publicPath2));
+
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
-app.get("/detalle", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/detalle.html"));
-});
-app.get("/carrito", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
-});
-app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/login.html"));
-});
-app.get("/registro", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/registro.html"));
-});
-app.get("/tienda", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/tienda.html"));
-});
 
-app.listen(3001, function() {
+app.listen(process.env.PORT || 3001, function() {
   console.log("Corriendo servidor en el puerto 3001");
 }
 )
