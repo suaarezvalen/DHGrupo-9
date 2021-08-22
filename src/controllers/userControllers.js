9a1210c167266bed05c7ba9fd26d505098545e46
Valentin Suarez <suarezvalentin2004@gmail.com>
2021-08-22 20:11:23 -0300

    agregando registroAccion, y modificando su vista

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,10 +1,55 @@
+const fs = require('fs');
+const path = require("path");
+
+const userFilePath = path.join(__dirname, "../database/user.JSON");
+let user = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
+
 const userControllers =  
 {
     login: (req, res) => {
         res.render('user/login')
     },
+
     registro: (req, res) => {
         res.render('user/registro')
+    },
+
+    registroAccion: (req, res) => {
+        let idNuevo = 0;
+
+        for(let i=0;i<user.length; i++) {
+            if(idNuevo<user[i].id){
+                idNuevo=user[i].id
+            }
+        }
+        
+        /*for (let i of user){
+            if (idNuevo<i.id){
+                idNuevo=i.id;
+            }
+        }*/                              //FOR DE JERO
+
+        idNuevo++;
+
+        let nombreImagen = req.file.filename
+
+        let userNuevo = {
+            id: idNuevo,
+            nombre: req.body.nombre,
+            mail: req.body.mail,
+            usuario: req.body.usuario,
+            contraseña: req.body.contraseña,
+            img: nombreImagen
+        }
+
+        user.push(userNuevo);
+
+        fs.writeFileSync(userFilePath, JSON.stringify(user, null, " "))
+
+        res.redirect("/product/tienda")
     }
+
+
+
 }   
 module.exports = userControllers;                                   
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,33 @@ const userControllers = require('./../controllers/userControllers')
 
 const express = require('express');
 const router = express.Router();
+const path = require("path")
+
+const multer = require("multer");
+
+/* MULTER */
+
+const configImagen = multer.diskStorage({
+    destination: function(req, file, cb) {
+     cb(null, path.join(__dirname,'../../public/img'));
+    },
+    filename: function(req, file, cb) {
+     let imageName =  Date.now() + file.originalname ;
+     cb(null, imageName);
+    }
+});
+
+const uploadFile = multer({storage: configImagen});
+
+/* MULTER */
+
+/* RUTAS */
 
 router.get('/registro', userControllers.registro);
+router.post('/registro', uploadFile.single("Imagen"), userControllers.registroAccion);
+
 router.get('/login', userControllers.login);
+
+/* RUTAS */
 
 module.exports = router;
