44cb1cec05238c275a5791c96aba043304c0c132
lucasangiorama <angioramaluca@gmail.com>
2021-09-19 20:32:31 -0300

    terminando session

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -33,16 +33,9 @@ router.post('/login',
     check('clave')
     ,userControllers.loginAccion)
 
-router.get('/usuario/:id', userControllers.usuarioData);
+router.get('/usuario', userControllers.usuarioData);
 
-router.get("/check", function(req, res){
-    if (req.session.usuarioLogueado == undefined){
-        res.send("no anduvo :p")
-    }else{
-        res.send("anduvo, bien ahi! ;)" + req.session.usuarioLogueado.mail)
-    }
-})
-
+router.get('/logout', userControllers.cerrarSession);
 /* RUTAS */
 
 module.exports = router;
